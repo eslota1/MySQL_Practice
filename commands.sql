e55896eb2d6fbc0456931a5eb8968cb11e446b73
eslota1 <elisabethslota0411@gmail.com>
2021-11-29 22:20:26 -0500

    the 10 queries

--- commands.sql
+++ commands.sql
@@ -24,7 +24,7 @@ CREATE TABLE House(
 CREATE TABLE BusinessProperty(
         address VARCHAR(50),
         ownerName VARCHAR(30),
-        price INT
+        price INT,
         type CHAR(20),
         size INT,
         FOREIGN KEY(address) REFERENCES Property(address)
@@ -47,7 +47,7 @@ CREATE TABLE Agent(
         FOREIGN KEY(firmId) REFERENCES Firm(id)
 );
 
-CREATE TABLE Listings(
+CREATE TABLE Listing(
         address VARCHAR(50),
         agentId INT,
         mlsNumber INT PRIMARY KEY,
@@ -75,3 +75,72 @@ CREATE TABLE Works_With(
         FOREIGN KEY(buyerId) REFERENCES Buyer(id),
         FOREIGN KEY(agentId) REFERENCES Agent(agentId)
 );
+
+-- 1
+SELECT Listing.address
+FROM House, Listing
+WHERE Listing.address = House.address;
+
+-- 2
+SELECT Listing.address, Listing.mlsNumber
+FROM House, Listing
+WHERE Listing.address = House.address;
+
+-- 3
+SELECT Listing.address
+FROM House, Listing
+WHERE Listing.address = House.address AND House.bedrooms = 3 AND House.bathrooms = 2;
+
+-- 4
+-- is this right?????
+SELECT address, price
+FROM House
+WHERE bedrooms = 3 AND bathrooms = 2 AND price >= 100000 AND price <= 250000
+ORDER BY price DESC;
+
+-- 5
+SELECT address, price
+FROM BusinessProperty
+WHERE type = 'office space'
+ORDER BY price DESC;
+
+-- 6
+SELECT agentId, Agent.name, phone, Firm.name, dateStarted
+FROM Agent, Firm
+WHERE Agent.firmId = Firm.id;
+
+-- 7
+-- put in agentId number
+SELECT Property.*
+FROM Property, Listing
+WHERE Property.address = Listing.address AND Listing.agentId = ?;
+
+--8
+SELECT Agent.name AS "Agent Name", Buyer.name AS "Buyer Name"
+FROM Agent, Buyer, Works_With
+WHERE Agent.agentId = Works_With.agentId AND Buyer.id = Works_With.buyerId
+ORDER BY Agent.name;
+
+-- 9
+SELECT Agent.agentId,
+(SELECT COUNT(Works_With.buyerId)
+FROM Works_With
+WHERE Agent.agentID = Works_With.agentId) AS "Number of Buyers Working with Agent"
+FROM Agent, Buyer, Works_With
+WHERE Agent.agentId = Works_With.agentId AND Buyer.id = Works_With.buyerId;
+
+
+-- 10
+-- put in Buyer.Id number
+SELECT House.*
+FROM House, Buyer
+WHERE Buyer.id = ? AND
+      Buyer.bedrooms = House.bedrooms AND
+      Buyer.bathrooms = House.bathrooms AND
+      Buyer.minimumPreferredPrice <= House.price AND
+      Buyer.maximumPreferredPrice >= House.price
+ORDER BY Property.price DESC;
+
+
+
+
