72564863b61c0ae8ad8a8a881a892caf07ba92f3
eslota1 <elisabethslota0411@gmail.com>
2021-12-02 18:30:38 -0500

    text file with query results

--- drop.sql
+++ drop.sql
@@ -1,7 +1,10 @@
-DROP TABLE `Agent`;
-DROP TABLE `Buyer`;
-DROP TABLE `Firm`;
-DROP TABLE `House`;
-DROP TABLE `Listings`;
-DROP TABLE `Property`;
-DROP TABLE `Works_With`;
+SET FOREIGN_KEY_CHECKS=0; -- to disable them
+DELETE FROM Agent;
+DELETE FROM Buyer;
+DELETE FROM Firm;
+DELETE FROM House;
+DELETE FROM Listings;
+DELETE FROM Property;
+DELETE FROM Works_With;
+DELETE FROM BusinessProperty;
+SET FOREIGN_KEY_CHECKS=1; -- to re-enable them
